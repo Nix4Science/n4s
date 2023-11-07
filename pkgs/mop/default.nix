009ee7b05554c2aa44178d3d8f4688c1799cbea0
Quentin Guilloteau <Quentin.Guilloteau@inria.fr>
2023-11-07 14:41:15 +0100

    call mop from its repo

--- pkgs/mop/default.nix
+++ pkgs/mop/default.nix
@@ -1,9 +1,14 @@
-{ python3Packages }:
+{ python3Packages, fetchFromGitHub }:
 
 python3Packages.buildPythonApplication rec {
     pname = "mop";
     version = "0.0.0";
-    src = ./.;
+    src = fetchFromGitHub {
+        owner = "Nix4Science";
+        repo = "mop";
+        rev = "v${version}";
+        sha256 = "sha256-vWeucqMeKL6OJWsTqfhIM53GQ/UqeiopBgrm70iuE8U=";
+    };
     propagatedBuildInputs = with python3Packages; [
         pyyaml requests
     ];
--- pkgs/mop/run.py
+++
@@ -1,3 +0,0 @@
-from app import *
-
-mop.main()
